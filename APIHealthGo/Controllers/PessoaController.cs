c048c22f7b10d3dccba31770a383b4f23d202d9f
Nícolas Santos <nicolasps.contato@gmail.com>
2025-06-13 16:39:09 -0300

    json config and route controller by id

--- APIHealthGo/Controllers/PessoaController.cs
+++ APIHealthGo/Controllers/PessoaController.cs
@@ -2,6 +2,7 @@
 using APIHealthGo.Response;
 using APIHealthGo.Services;
 using atividade_bd_csharp.Entity;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MyFirstCRUD.DTO;
 using MyFirstCRUD.Repository;
@@ -10,6 +11,7 @@ namespace APIHealthGo.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    [Authorize]
     public class PessoaController : ControllerBase
     {
         private IPessoaService _service;
@@ -33,9 +35,9 @@ namespace APIHealthGo.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<MessageResponse>> Post(PessoaInsertDTO mecanico)
+        public async Task<ActionResult<MessageResponse>> Post(PessoaInsertDTO pessoa)
         {
-            return Ok(await _service.Post(mecanico));
+            return Ok(await _service.Post(pessoa));
         }
 
         [HttpPut]
